2073173edf7bad2fcdab139f2526de49d3b28b20
ernestvp <vidaurrer87@gmail.com>
2026-05-09 09:44:43 +0200

    fix  database config for railway

--- app/app/Config/Database.php
+++ app/app/Config/Database.php
@@ -25,31 +25,24 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+    'DSN'          => '',
+    'hostname'     => $_ENV['DB_HOST'] ?? 'db',
+    'username'     => $_ENV['DB_USERNAME'] ?? 'usuario',
+    'password'     => $_ENV['DB_PASSWORD'] ?? 'password',
+    'database'     => $_ENV['DB_DATABASE'] ?? 'control_visitas',
+    'DBDriver'     => 'MySQLi',
+    'DBPrefix'     => '',
+    'pConnect'     => false,
+    'DBDebug'      => true,
+    'charset'      => 'utf8mb4',
+    'DBCollat'     => 'utf8mb4_general_ci',
+    'swapPre'      => '',
+    'encrypt'      => false,
+    'compress'     => false,
+    'strictOn'     => false,
+    'failover'     => [],
+    'port'         => (int)($_ENV['DB_PORT'] ?? 3306),
+];
 
     //    /**
     //     * Sample database connection for SQLite3.
